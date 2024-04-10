c6a4086411b1ce37721832792f2d5b54a6561a7c
Alexsander Sudario <92872511+alexsander020@users.noreply.github.com>
2024-04-09 21:43:49 -0300

    Tela de Loading

---
+++ app/src/main/java/com/example/comedoria/LoadingActivity.java
@@ -0,0 +1,24 @@
+package com.example.comedoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class LoadingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
