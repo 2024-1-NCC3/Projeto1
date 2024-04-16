1a0674b5af69723246975e1d6d0453cde26ae857
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2024-04-16 19:26:07 -0300

    RecycleView

---
+++ FrontEnd/app/src/main/java/br/edu/fecap/comedoriatia/Adapter.java
@@ -0,0 +1,10 @@
+package br.edu.fecap.comedoriatia;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    public class  MyViewHolder
+
+
+}
