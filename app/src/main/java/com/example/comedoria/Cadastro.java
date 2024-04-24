43b64d8df4a22af1ceaf73d53114df68c9941601
Bruno Costa Dourado <bruno.dourado@edu.fecap.br>
2024-04-23 22:19:48 -0300

    back tela de cadastro

--- app/src/main/java/com/example/comedoria/Cadastro.java
+++ app/src/main/java/com/example/comedoria/Cadastro.java
@@ -1,12 +1,22 @@
 package com.example.comedoria;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Cadastro extends AppCompatActivity {
+
+    private EditText inputNome, inputSobrenome, inputCpf, inputEmail, inputSenha;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro);
+
+        inputCpf = findViewById(R.id.txtCpf);
+        inputEmail = findViewById(R.id.txtEmail);
+        inputSenha = findViewById(R.id.txtSenha);
     }
 }
