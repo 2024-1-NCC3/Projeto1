af9c9b9477ed7562eac5b5fd768022a7bf17e115
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2024-04-29 19:53:05 -0300

    conclusao de resumo_pedido
    Concluido:
    AdaperResumo
    layoutResumpedido
    
    A fazer:
    Chamada do servidor ao pedido

--- app/src/main/java/com/example/comedoria/AdapterResumoPedido.java
+++ app/src/main/java/com/example/comedoria/AdapterResumoPedido.java
@@ -1,33 +1,73 @@
 package com.example.comedoria;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class AdapterResumoPedido extends RecyclerView.Adapter<AdapterResumoPedido.MyViewHolder> {
+public class AdapterResumoPedido extends RecyclerView.Adapter<AdapterResumoPedido.MyviewHolder> {
 
-    private List<Produto> listaResumo;
+    private List<Produto> listaCarrinho;
     private Context contextResumo;
 
     @NonNull
     @Override
-    public AdapterCarrinho.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+    public MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         View itemLista = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.layout_resumo_pedido, parent,false);
-        return new AdapterResumoPedido.MyViewHolder(itemLista);
+        return new AdapterResumoPedido.MyviewHolder(itemLista);
 
     }
-
     public AdapterResumoPedido(List<Produto> lista, Context contextResumo){
-        this.listaResumo = lista;
+        this.listaCarrinho = lista;
         this.contextResumo = contextResumo;
     }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterResumoPedido.MyviewHolder holder, int position) {
+        Produto produto = listaCarrinho.get(position);
+
+        holder.nomeProduto.setText((produto.getNome()));
+        holder.quantidadeProduto.setText((produto.getQuantidade()));
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyviewHolder extends RecyclerView.ViewHolder{
+
+        TextView nomeProduto;
+        TextView quantidadeProduto;
+
+
+
+
+
+        @SuppressLint("ResourceAsColor")
+        public MyviewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeProduto = itemView.findViewById(R.id.descricaoProduto);
+            quantidadeProduto = itemView.findViewById(R.id.qntProduto);
+        }
+    }
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/comedoria/ComprovantePedido.java
+++ app/src/main/java/com/example/comedoria/ComprovantePedido.java
@@ -1,14 +1,56 @@
 package com.example.comedoria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ComprovantePedido extends AppCompatActivity {
+
+    TextView textData, tituloPedido, statusPedido;
+
+    RecyclerView recyclerResumo;
+
+    List<Produto> produtos;
+
+    int dia, mes;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comprovante_pedido);
+
+        String arrayProdutos = getIntent().getStringExtra("produtosSelecionados");
+        //produtos = Arrays.asList(new Gson().fromJson(arrayProdutos, Produto[].class));
+        textData = findViewById(R.id.textData);
+        tituloPedido = findViewById(R.id.tituloPedido);
+        statusPedido = findViewById(R.id.statusPedido);
+
+        recyclerResumo = findViewById(R.id.recycleResumo);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerResumo.setLayoutManager(layoutManager);
+        recyclerResumo.setHasFixedSize(true);
+
+        AdapterResumoPedido adapterResumoPedido = new AdapterResumoPedido(produtos, this);
+        recyclerResumo.setAdapter(adapterResumoPedido);
+
+
+
     }
+
+
+
+
+
 }
