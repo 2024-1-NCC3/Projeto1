d7d5612227c7927c0c13f1fe63e84112d8b88d6d
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2024-05-06 15:58:02 -0300

    Tela de Estoque - Não Finalizada

---
+++ app/src/main/java/com/example/comedoria/AdapterEstoque.java
@@ -0,0 +1,70 @@
+package com.example.comedoria;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterEstoque extends RecyclerView.Adapter<AdapterEstoque.ViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private android.content.Context context;
+    private List<Produto> listaProdutos;
+
+
+    AdapterEstoque(Context context, List<Produto> lista){
+        this.context = context;
+        this.listaProdutos = lista;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.modelo_estoque, viewGroup, false);
+        return new ViewHolder(itemLista);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Produto produto = listaProdutos.get(i);
+        viewHolder.textTitle.setText(produto.getNome());
+        viewHolder.textTitle.setText(produto.getNome());
+        Picasso.get().load(produto.getCaminhoImg()).into(viewHolder.imgProduto);
+
+        viewHolder.textPreco.setText(String.format(Locale.getDefault(), "R$ %.2f", produto.getPreco()) );
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaProdutos.size();
+    }
+
+    public List<Produto> getListaProdutos(){
+        return this.listaProdutos;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textTitle, textQuantidade, textPreco;
+        ImageView imgProduto;
+
+        @SuppressLint("ResourceAsColor")
+        public ViewHolder(View itemView){
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitulo);
+            textQuantidade = itemView.findViewById(R.id.textQuantidade);
+            textPreco = itemView.findViewById(R.id.textPreco);
+            imgProduto = itemView.findViewById(R.id.imgProduto);
+        }
+    }
+}
--- app/src/main/java/com/example/comedoria/Estoque.java
+++ app/src/main/java/com/example/comedoria/Estoque.java
@@ -1,14 +1,95 @@
 package com.example.comedoria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Estoque extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    String accessToken;
+    AdapterEstoque adapterEstoque;
+    private List<Produto> listaProdutos = new ArrayList<>();
 
+    @SuppressLint({"ResourceAsColor", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estoque);
+
+        recyclerView = findViewById(R.id.recyclerEstoque);
+        accessToken = getIntent().getStringExtra("accessToken");
+
+        acessarListaProdutos();
+
+        adapterEstoque = new AdapterEstoque(this, listaProdutos);
+        RecyclerView.LayoutManager layoutManager1 = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(layoutManager1);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void acessarListaProdutos(){
+        Map<String, String> headers = new HashMap<>();
+        //define os heades que a solicitação vai precisar
+        headers.put("apikey", API_KEY);
+        headers.put("Authorization", "Bearer " + accessToken);
+
+        ConectorAPI.conexaoArrayGET(
+                "/rest/v1/produtos?select=*,categoria(nome_categoria)",
+                headers,
+                getApplicationContext(),
+                new ConectorAPI.VolleyArrayCallback() {
+                    @Override
+                    public void onSuccess(JSONArray response) throws JSONException {
+                        if(response.length() > 0){
+                            for(int i = 0; i< response.length();i++){
+                                JSONObject jsonObject = response.getJSONObject(i);
+
+                                int id = jsonObject.getInt("id_produto");
+                                String nomeProduto = jsonObject.getString("nome_produto");
+                                Double preco = jsonObject.getDouble("preco");
+                                String caminhoImagem = jsonObject.getString("caminho_imagem");
+
+                                //Pegas as categorias e armazena em uma lista;
+
+                                JSONArray arrayCategorias = jsonObject.getJSONArray("categoria");
+
+                                List<String> categorias = new ArrayList<>();
+
+                                if(arrayCategorias.length() >0){
+                                    for(int j = 0; j<arrayCategorias.length();j++){
+                                        JSONObject cat = arrayCategorias.getJSONObject(j);
+                                        categorias.add(cat.getString("nome_categoria"));
+                                    }
+                                }
+
+                                listaProdutos.add(new Produto(id,nomeProduto,preco,categorias,caminhoImagem));
+                            }
+                        }
+                        adapterEstoque.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onError(VolleyError error) {
+
+                    }
+                });
     }
 }
